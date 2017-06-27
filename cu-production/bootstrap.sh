b67ccd483868a5c972f49fbca56bd089edd031fc
Michaël GRILLET <m.grillet@treeptik.fr>
2017-06-27 14:49:35 +0200

    Add proxy configuration for production

--- cu-production/bootstrap.sh
+++ cu-production/bootstrap.sh
@@ -237,6 +237,31 @@ logo_pulling_dockerhub() {
 echo "Pulling CloudUnit..."
 }
 
+question_proxy() {
+	echo ""
+	echo "Configure proxy (default is [no])"
+	echo "( yes / no ) : "
+	read USE_PROXY
+	if [ "$USE_PROXY" = "yes" ]; then
+	  set_proxy
+	else
+	  echo "No configured proxy."
+	fi
+}
+
+set_proxy() {
+	echo ""
+	echo "Enter proxy address : "
+	read PROXY
+	if [ -n "$PROXY" ]; then
+		export http_proxy=$PROXY
+		export https_proxy=$http_proxy
+		export ftp_proxy=$http_proxy
+	else
+		echo "No configured proxy."
+	fi
+}
+
 #
 #
 # MAIN
@@ -250,6 +275,8 @@ if [ -f /etc/redhat-release ]; then
 else
   distribution=$(cat /etc/issue | cut -c1-6)
 fi
+
+question_proxy
 
 check_git_branch
 
